11a90f34e56626186399533ef7aabe186e5af37f
M0nkeyPr0grammer <randoriumknights@gmail.com>
2023-10-28 13:13:13 -0500

    Add project files.

---
+++ src/client/java/m0nkeypr0grammer/emi/grouping/Emi_groupingClient.java
@@ -0,0 +1,10 @@
+package m0nkeypr0grammer.emi.grouping;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Emi_groupingClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
